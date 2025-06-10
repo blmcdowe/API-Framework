c9505b736245fae874d89e46d0f445c32e0547f0
Byron McDowell <bgroot2021@gmail.com>
2025-06-10 01:24:28 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,11 @@
+BASE_URL = "https://jsonplaceholder.typicode.com"
+
+REQUEST_TIMEOUT = 10
+
+DEFAULT_HEADERS = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+MAX_RETRIES = 3
+RETRY_DELAY = 2  # seconds
